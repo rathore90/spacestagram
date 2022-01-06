bfbbd2f20cf8568dd8919989157c6565ba744d3d
Pardeep <pardeep@sweetlegs.ca>
2022-01-06 12:29:50 -0800

    fix console error and add more styling

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -4,24 +4,26 @@ import Reactions from "./Reactions";
 export default function DataContainer(props) {
   return (
     <div className="nasa-photo">
-      {props.data.media_type === "image" ? (
-        <img
-          src={props.data.url}
-          alt={props.data.title}
-          className="photo"
-        />
-      ) : (
-        <iframe
-          title="space-video"
-          src={props.data.url}
-          frameBorder="0"
-          gesture="media"
-          allow="encrypted-media"
-          allowFullScreen
-          className="photo"
-        />
-      )}
-      <div>
+      <div className="img-container">
+        {props.data.media_type === "image" ? (
+          <img
+            src={props.data.url}
+            alt={props.data.title}
+            className="photo"
+          />
+        ) : (
+          <iframe
+            title="space-video"
+            src={props.data.url}
+            frameBorder="0"
+            gesture="media"
+            allow="encrypted-media"
+            allowFullScreen
+            className="photo"
+          />
+        )}
+      </div>
+      <div className="data-container">
         <h1>{props.data.title}</h1>
         <p className="date">{props.data.date}</p>
         <p className="explanation">{props.data.explanation}</p>
