ce19c8796255a59c55aa1f490839d870e4d0d895
Pardeep <pardeep@sweetlegs.ca>
2022-01-07 10:19:11 -0800

    added loading spinner

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -4,16 +4,19 @@ import DataContainer from "./DataContainer";
 import Footer from "./Footer";
 import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
 import "./../App.css";
+import { ClimbingBoxLoader } from "react-spinners";
 
 
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState(null);
   const [query, setQuery] = useState(['2022-01-02', '2022-01-06']);
+  const [loading, setLoading] = useState(false);
 
   const onChange = (props) => {
     const stateDate = props.startDate;
     const endDate = props.endDate;
 
+    // date formatting, this could have been done with the package, this is optional
     let startdate = new Date(stateDate);
     let startdate_ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(startdate);
     let startdate_mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(startdate);
@@ -30,14 +33,15 @@ export default function NasaPhoto() {
   };
   
   useEffect(() => {
+    setLoading(true);
     fetchPhoto();
-
     async function fetchPhoto() {
       const res = await fetch(
         `https://api.nasa.gov/planetary/apod?start_date=${query[0]}&end_date=${query[1]}&api_key=k2XWlRUP28NUg4Zhwt8jbgiqKbI8Mdfb6Shni0Gf`
       );
       const data = await res.json();
       setPhotoData(data);
+      setLoading(false);
     }
   }, [query]);
 
@@ -45,18 +49,27 @@ export default function NasaPhoto() {
 
   return (
     <>
-      <NavBar />
-      <div className="datepicker-wrapper"> 
-        <DateRangePickerComponent 
-          placeholder="Please enter a date range" 
-          change={ onChange }/>
-      </div>
-        {
-          photoData.map(photodata => {
-            return <DataContainer id="datepicker" cssClass="e-custom-style" data={photodata} key={ photodata.date }/>
-          })
-        }
-      <Footer/>
+      { loading ? 
+        (<ClimbingBoxLoader size={30} color={"#d14f4f"} loading={loading}/>) : 
+        (<>
+          <NavBar />
+          <div className="datepicker-wrapper">
+            <DateRangePickerComponent
+              placeholder="Please enter a date range"
+              change={onChange} />
+          </div>
+          {
+            photoData.map(photodata => {
+              return <DataContainer id="datepicker" cssClass="e-custom-style" data={photodata} key={photodata.date} />
+            })
+          }
+          <Footer />
+        </>
+        )
+}
+
+
+
     </>
   );
 }
