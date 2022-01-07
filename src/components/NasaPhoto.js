1b9695b6e32144eff0528075cf184a652b9c337b
Pardeep <pardeep@sweetlegs.ca>
2022-01-07 08:24:30 -0800

    added date range, calendar, and fix responsive issue

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -2,33 +2,60 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import DataContainer from "./DataContainer";
 import Footer from "./Footer";
+import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
+import "./../App.css";
 
 
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState(null);
+  const [query, setQuery] = useState(['2022-01-02', '2022-01-06']);
 
+  const onChange = (props) => {
+    const stateDate = props.startDate;
+    const endDate = props.endDate;
+
+    let startdate = new Date(stateDate);
+    let startdate_ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(startdate);
+    let startdate_mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(startdate);
+    let startdate_da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(startdate);
+    let formattedSD = `${startdate_ye}-${startdate_mo}-${startdate_da}`;
+
+    let enddate = new Date(endDate);
+    let enddate_ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(enddate);
+    let enddate_mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(enddate);
+    let enddate_da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(enddate);
+    let formattedESD = `${enddate_ye}-${enddate_mo}-${enddate_da}`;
+
+    setQuery([formattedSD, formattedESD]);
+  };
+  
   useEffect(() => {
     fetchPhoto();
 
     async function fetchPhoto() {
       const res = await fetch(
-        `https://api.nasa.gov/planetary/apod?start_date=2022-01-01&end_date=2022-01-06&api_key=k2XWlRUP28NUg4Zhwt8jbgiqKbI8Mdfb6Shni0Gf`
+        `https://api.nasa.gov/planetary/apod?start_date=${query[0]}&end_date=${query[1]}&api_key=k2XWlRUP28NUg4Zhwt8jbgiqKbI8Mdfb6Shni0Gf`
       );
       const data = await res.json();
       setPhotoData(data);
     }
-  }, []);
+  }, [query]);
 
   if (!photoData) return <div />;
 
   return (
     <>
       <NavBar />
-      {
-        photoData.map(photodata => {
-          return <DataContainer data={photodata} key={ photodata.date }/>
-        })
-      }
+      <div className="datepicker-wrapper"> 
+        <DateRangePickerComponent 
+          placeholder="Please enter a date range" 
+          change={ onChange }/>
+      </div>
+        {
+          photoData.map(photodata => {
+            return <DataContainer id="datepicker" cssClass="e-custom-style" data={photodata} key={ photodata.date }/>
+          })
+        }
       <Footer/>
     </>
   );
